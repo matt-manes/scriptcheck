6c5281171ab88b2989897b1d7c6a69857d289471
matt manes <mattmanes@pm.me>
2023-11-02 12:14:08 -0500

    Initial commit

---
+++ tests/test_scriptcheck.py
@@ -0,0 +1,10 @@
+import pytest
+
+from scriptcheck import scriptcheck
+
+def test__get_scripts():
+    print()
+    scripts = scriptcheck.get_scripts()
+    assert isinstance(scripts, list)
+    assert len(scripts) > 0
+    print(*scripts, sep="\n")
